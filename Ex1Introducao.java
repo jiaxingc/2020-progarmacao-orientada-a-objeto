8597524847466495dd605ac0d00a1a5463a982f6
jiaxingc <43052057+jiaxingc@users.noreply.github.com>
2020-08-25 20:42:32 -0300

    Create Ex1Introducao.java

---
+++ Ex1Introducao.java
@@ -0,0 +1,13 @@
+public class Main{
+  public static void main(String[] args){
+    int i=0,j=0;
+    int x =0;
+    for(i=0;i<10;i++){
+      for(j=0;j<10;j++){
+        x=i*j;
+        System.out.println(i +"*"+ j +"="+x);
+      }
+    }
+    
+  }
+ }
