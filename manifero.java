67e5c5133249758d944e3f971e5eebb331b0b5c4
jiaxingc <43052057+jiaxingc@users.noreply.github.com>
2020-09-28 19:36:43 -0300

    Create manifero.java

---
+++ manifero.java
@@ -0,0 +1,20 @@
+public class canguru extends mamifero{
+   String variedade;
+  public canguru(){
+    //是否引用mamiforo的元素
+   variedade="canguru-vermelho";}
+
+   
+    
+    public void locomover()
+    {
+      System.out.println("O canguru esta movimentando para esqueda");
+    }
+    public void alimentar(){
+       System.out.println("O canguru esta comendo cenoura");
+    }
+    public void emitirSom(){
+      System.out.println("gejigeji");
+    }
+  }
+
